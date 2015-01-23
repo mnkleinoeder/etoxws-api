aa61157542e3a3e0219c85a12a13c5ae2417de7b
Thomas Kleinoeder <kleinoeder.github@molecular-networks.com>
2015-01-23 21:23:33 +0100

    missing fix for user pypath

--- deploy/roles/etoxws-server/templates/make_env.sh
+++ deploy/roles/etoxws-server/templates/make_env.sh
@@ -1,14 +1,15 @@
 . {{etoxws_virtenv}}/bin/activate
 
 export LD_LIBRARY_PATH={{RDKIT_DIR}}/lib
-export PYTHONPATH={{ETOXWS_APPDIR}}/src:/opt/rdkit/lib/python{{python_version}}/site-packages
+pp=""
 {% for p in (ETOXWS_IMPL_V1.PYPATH, ETOXWS_IMPL_V2.PYPATH) %}
 {% if p|length > 0 %}
-export PYTHONPATH=$PYTHONPATH:{{p}}
+pp={{p}}${pp:+:${pp}}
 {% endif %} 
 {% endfor %} 
+
+export PYTHONPATH=${pp:+${pp}:}{{ETOXWS_APPDIR}}/src:/opt/rdkit/lib/python{{python_version}}/site-packages
 export DJANGO_SETTINGS_MODULE=etoxwsapi.settings
 
-cd {{ETOXWS_APPDIR}}/src/etoxwsapi
-
-echo run django with "python manage.py <action>"
+#echo {{ETOXWS_APPDIR}}/src/etoxwsapi
+#echo run django with "python manage.py <action>"
